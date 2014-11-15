b33e5d99a5ff210d47505f9b229e8df18be47536
koenvinken <koen.vinken@wijs.be>
2014-11-15 12:37:10 +0100

    create blog entity

---
+++ src/Blogger/BlogBundle/Entity/Blog.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Blogger\BlogBundle\Entity;
+
+class Blog
+{
+    protected $title;
+
+    protected $author;
+
+    protected $blog;
+
+    protected $image;
+
+    protected $tags;
+
+    protected $comments;
+
+    protected $created;
+
+    protected $updated;
+}
