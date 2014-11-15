21e7e72b42b448e65e33cb8e338b8565f3aca57e
koenvinken <koen.vinken@wijs.be>
2014-11-15 11:23:55 +0100

    add contact page

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -14,4 +14,9 @@ class PageController extends Controller
     {
         return $this->render('BloggerBlogBundle:Page:about.html.twig');
     }
+
+    public function contactAction()
+    {
+        return $this->render('BloggerBlogBundle:Page:contact.html.twig');
+    }
 }
