3f48809e78669f96117b1b59b8a05359d504d414
koenvinken <koen.vinken@wijs.be>
2014-11-15 12:08:50 +0100

    add swift mailer

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -27,7 +27,14 @@ class PageController extends Controller
             $form->bind($request);
 
             if ($form->isValid()) {
-                // Perform some action, such as sending an email
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact enquiry from symblog')
+                    ->setFrom('enquiries@symblog.co.uk')
+                    ->setTo('email@email.com')
+                    ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                $this->get('mailer')->send($message);
+
+                $this->get('session')->setFlash('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
 
                 // Redirect - This is important to prevent users re-posting
                 // the form if they refresh the page
